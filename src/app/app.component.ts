bdab9af8d848c55af9381ff7c7e2eee13d3e329a
Alberto Peral <info@peralstudio.com>
2020-08-14 12:12:50 +0200

    edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 // Firebase
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { finalize } from 'rxjs/operators';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs/internal/observable';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { firestore } from "firebase/app";
 // Models
 import { User } from './models/user';
 import { v4 as uuidv4 } from 'uuid';
@@ -27,6 +28,11 @@ export class AppComponent implements OnInit {
     url: string | ArrayBuffer;
     myform: any;
 
+    // variable para guardar la url vieja de la foto temporalmente
+    antiguoFilename: string
+    userID: string
+
+
     constructor(
         private toastr: ToastrService,
         private storage: AngularFireStorage,
@@ -45,6 +51,7 @@ export class AppComponent implements OnInit {
     //arreglos
     userList1: User[] = []
     useList2: User[] = []
+    mode: 'add' | 'edit' = 'add'
 
 
     @ViewChild('imageUser') inputImageUser: ElementRef;
@@ -60,12 +67,11 @@ export class AppComponent implements OnInit {
 
     delete(users) {
         //delete archivo Storage
-        console.log('delete',users.filename)
+        console.log('delete', users.filename)
         this.storage.ref(users.filename).delete();
         //delete document Database
         this.db.doc(`/users/${users.id}`).delete().then(e => {
             this.toastr.error(`Usuario: ${users.nombre}, ${users.apellido} Eliminado`);
-            this.loadUsers();
         })
 
     }
@@ -73,35 +79,39 @@ export class AppComponent implements OnInit {
     //FUNCION EDITAR
 
     edit(users) {
+        this.mode = 'edit'
+        // ahora vas al html 
         window.scrollTo({
             top: 0,
             left: 0,
             behavior: 'smooth'
         });
-        this.toastr.error(`Editando Usuario: ${users.nombre}, ${users.apellido}`);
+        this.toastr.info(`Editando Usuario: ${users.nombre}, ${users.apellido}`);
         console.log(users.nombre, users.apellido)
         console.log(users.image)
         this.userForm.patchValue({
             firstName: users.nombre,
             lastName: users.apellido,
             access: users.access,
-            /*imagePost: {
-                src: {
-                    url: users.image
-                }
-            }*/
         });
+        this.url = users.image;
+        this.antiguoFilename = users.filename;
+        this.userID = users.id;
+
+        // 
+        this.userForm.get("imagePost").clearValidators();
+        this.userForm.get("imagePost").updateValueAndValidity()
     }
 
     //CAPTURAR RUTA IMAGEN
-    
+
     onUpload(e) {
         const data = e.target.files[0];
         this.file = data
         console.log(data.name)
         const extension = data.name.split('.')[1].toLowerCase()
         this.id = `uploads/${uuidv4()}.${extension}`
-        console.log('RUTA IMAGEN',this.id)
+        console.log('RUTA IMAGEN', this.id)
     }
 
     private createForm(): FormGroup {
@@ -114,18 +124,31 @@ export class AppComponent implements OnInit {
     }
 
     loadUsers() {
-        this.db.collection<User>('users').get().subscribe((result) => {
-            this.userList = result.docs.map(item => { return <User>({ id: item.id, ...item.data() }) });
-        })
+        this.db.collection<User>('users', x => x.orderBy('createdAt', 'desc')).valueChanges()
+            .subscribe((result) => {
+                this.userList = result;
+                console.log(result)
+            })
 
     }
 
     cloudStorage(nombreArchivo: string) {
+
         return this.storage.ref(nombreArchivo)
     }
 
-    sendCloudStorage(nombreArchivo: string, datos: any) {
-        return this.storage.upload(nombreArchivo, datos)
+    sendCloudStorage(nombreArchivo: string, image: any) {
+        // comenzemos por aqui.
+        // necesitamos usar el flag aqui. para que si se edita la imagen no se suba otra sino que actualize la que ya se subio.
+        // eso es practicamente lo mismo que se hace con la data.
+        // segun el flag agregas o actualizas
+        if (this.mode == 'add') {
+            return this.storage.upload(nombreArchivo, image)
+        }
+        else {
+            // este el el metodo de actualizar en firebase.ok, aqui me he perdido un poco mas pero ya estudiare esta parte ;)
+            return this.storage.ref(nombreArchivo).put(image)
+        }
     }
 
     addNewUser() {
@@ -134,37 +157,86 @@ export class AppComponent implements OnInit {
             const auxUser = this.userForm.value as User;
             //obtengo dato del usuario
             //referencia del archivo
-            let ref = this.cloudStorage(this.id)
-            let task = this.sendCloudStorage(this.id, this.file)
-            task.percentageChanges().subscribe(porcentaje => {
-                this.porcentaje = Math.round(porcentaje)
-                this.uploadPercent = this.porcentaje
-                if (this.porcentaje == 100) {
-                    console.log("finalizado")
-                    //esperar a que este finalizado para obtener el url
-                    ref.getDownloadURL().subscribe(async (URL) => {
 
-                        const param = {
-                            nombre: auxUser.firstName,
-                            apellido: auxUser.lastName,
-                            image: URL,
-                            access: auxUser.access,
-                            filename: this.id
-                        }
-                        //envio el objeto a firebase
-                        await this.db.collection('users').add(param).then(() => {
-                            this.loadUsers();
-                            this.uploadPercent = 0;
-                            this.userForm.reset();
-                            this.url = "";
-                            this.toastr.success(`Usuario: ${param.nombre}, ${param.apellido} Guardado`);
+            const identificador = this.mode == 'add' ? this.id : this.antiguoFilename
+
+            if (this.file) {
+                let ref = this.cloudStorage(identificador)
+                let task = this.sendCloudStorage(identificador, this.file)
+                task.percentageChanges().subscribe(porcentaje => {
+                    this.porcentaje = Math.round(porcentaje)
+                    this.uploadPercent = this.porcentaje
+                    if (this.porcentaje == 100) {
+                        console.log("finalizado")
+                        //esperar a que este finalizado para obtener el url
+                        ref.getDownloadURL().subscribe(async (url) => {
+                            this.editData(auxUser, url, identificador)
                         })
-                            .catch()
-                    })
-                }
-            })
+                    }
+                })
+            }
+            else {
+                this.editData(auxUser, null, identificador)
+            }
 
         }
+    }
+
+    editData(auxUser: any, url: string, identificador: string) {
+        const docID = this.mode == 'add' ? uuidv4() : this.userID;
+        let param: any = {}
+        console.log('URL', url)
+        if (url) {
+            param = {
+                id: docID,
+                createdAt: firestore.FieldValue.serverTimestamp(),
+                nombre: auxUser.firstName,
+                apellido: auxUser.lastName,
+                image: url,
+                filename: identificador,
+                access: auxUser.access,
+            }
+        }
+        else {
+            param = {
+                id: docID,
+                nombre: auxUser.firstName,
+                updatedAt: firestore.FieldValue.serverTimestamp(),
+                apellido: auxUser.lastName,
+                // image: url,
+                // filename: identificador, 
+                access: auxUser.access,
+            }
+        }
+        // const param = {
+        //     id : docID,
+        //     nombre: auxUser.firstName,
+        //     apellido: auxUser.lastName,
+        //     ...( this.url ? {
+        //         image: url,
+        //         filename: identificador
+        //     } : {}),
+        //     access: auxUser.access,
+        // }
+
+        //envio el objeto a firebase
+        let result: Promise<any>
+        const docRef = this.db.doc('users/' + docID).ref
+
+        result = this.mode == 'add' ? docRef.set(param) : docRef.update(param)
+
+        result.then(() => {
+            this.uploadPercent = 0;
+            this.userForm.reset();
+            this.url = "";
+            this.userID = ""
+            this.antiguoFilename = ""
+            this.file = null;
+            this.mode = 'add';
+
+            this.toastr.success(`Usuario: ${param.nombre}, ${param.apellido}  ${this.mode == 'add' ? 'Guardado' : 'Actualizado'}`);
+        })
+            .catch()
     }
 
     userListAccess(access: 'yes' | 'no'): User[] {
@@ -198,6 +270,13 @@ export class AppComponent implements OnInit {
     }
 
     reset() {
+        this.uploadPercent = 0;
+        this.userForm.reset();
+        this.url = "";
+        this.userID = ""
+        this.antiguoFilename = ""
+        this.file = null;
+        this.mode = 'add';
         this.url = "";
     }
 
